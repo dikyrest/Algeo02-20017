588f649505bf1f62a3bfd58763059fa7bfa7a9a7
Hilya Fadhilah Imania <hilyafadhilah@gmail.com>
2021-11-10 02:28:41 +0700

    add client functionality

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,5 +1,82 @@
 <script setup>
+import { NConfigProvider, NGlobalStyle } from 'naive-ui'
+import Main from './components/Main.vue'
 </script>
 
 <template>
+  <NConfigProvider abstract>
+    <NGlobalStyle />
+    <div class="view">
+      <header>
+        <h1>Kompres Air&nbsp;Hangat</h1>
+        <p>Kompresor&nbsp;gambar yang&nbsp;sangat&nbsp;baik.</p>
+      </header>
+      <Main />
+      <footer>
+        <div class="footer-content">&copy; Hak Cipta 2021</div>
+      </footer>
+    </div>
+  </NConfigProvider>
 </template>
+
+<style lang="scss">
+@use "normalize.css/normalize.css";
+
+$footer-height: 36px;
+
+body {
+  min-height: 100vh;
+  position: relative;
+}
+
+#app {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+
+  .view {
+    flex-basis: 1080px;
+    padding-bottom: $footer-height;
+    display: flex;
+    flex-direction: column;
+
+    &,
+    & .footer-content {
+      margin: 0 25px;
+    }
+  }
+}
+
+header {
+  text-align: center;
+  margin-top: 10vmin;
+  margin-bottom: 30px;
+
+  h1 {
+    margin: 0;
+    line-height: 1;
+    font-size: 2.5rem;
+  }
+
+  p {
+    margin: 10px 0 0 0;
+  }
+}
+
+footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 36px;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+
+  .footer-content {
+    white-space: nowrap;
+  }
+}
+</style>
---
+++ src/client/src/components/FileInput.vue
@@ -0,0 +1,107 @@
+<script setup>
+import { NUpload, NUploadDragger, NIcon, NH2, NP, useMessage } from 'naive-ui'
+import { ImageAdd24Regular as Icon } from '@vicons/fluent'
+import { ref } from '@vue/reactivity'
+
+const emit = defineEmits(['change'])
+
+const types = ['image/png', 'image/jpeg', 'image/gif']
+const draggerClass = ref('')
+const message = useMessage()
+
+const beforeUpload = async ({ file }) => {
+  if (!types.includes(file.file.type)) {
+    message.error(`Format file ${file.file.type} tidak didukung!`)
+    draggerClass.value = 'dragger-error'
+
+    setTimeout(() => {
+      draggerClass.value = ''
+    }, 850)
+
+    return false
+  }
+
+  return true
+}
+</script>
+
+<template>
+  <NUpload :show-file-list="false" @before-upload="beforeUpload" @change="emit('change', $event)">
+    <NUploadDragger :class="draggerClass">
+      <NIcon size="48">
+        <Icon />
+      </NIcon>
+      <NH2 style="margin: 10px 0 0 0;">Unggah gambar&nbsp;Anda di sini</NH2>
+      <NP depth="3" style="margin: 10px 0 0 0;">
+        Tekan&nbsp;di&nbsp;sini
+        atau&nbsp;geser&nbsp;file&nbsp;gambar&nbsp;Anda
+        ke&nbsp;area&nbsp;ini
+      </NP>
+      <NP style="margin: 5px 0 0 0;">Format yang didukung: JPEG,&nbsp;PNG,&nbsp;dan&nbsp;GIF</NP>
+    </NUploadDragger>
+  </NUpload>
+</template>
+
+<style lang="scss">
+.n-spin-container,
+.n-spin-content,
+.n-upload {
+  width: 100%;
+}
+
+.n-upload {
+  display: flex;
+  justify-content: center;
+}
+
+.n-upload-trigger {
+  flex-basis: 500px;
+}
+
+.n-upload-dragger {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<style scoped lang="scss">
+.dragger-error {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+
+  &,
+  &:hover {
+    /* TODO: colors */
+    border-color: red;
+    background-color: coral;
+  }
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
+</style>
